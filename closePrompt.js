1de20bb856742f3248bd51f5f7fc43d23cfc2548
beni529 <Brkicbeni@Gmail.com>
2025-04-20 20:10:32 -0700

    idk

--- closePrompt.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("clipboard", function (e) {
-    var confirmationMessage = 'It looks like you have been editing something. '
-                            + 'If you leave before saving, your changes will be lost.';
-
-    console.log("AAA");
-    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-});
-
-console.log("a")
